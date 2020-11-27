7e3f3b1140d653ee7aee6a94a0f9691ef6948ad4
Stefanie Nader <stefanie.nader1992@web.de>
2020-11-27 11:58:42 +0100

    without CurrentDay

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ function init() {
         let id = 'door' + i;
         document.getElementById(id).innerHTML = `<h1>${i}</h1>`;
     }
+
+    /*let currentDay = new Date().getDate();
+
+    for (let i = 1; i <=24; i++) {
+        let id ='door' + i;
+
+        document.getElementById(id).innerHTML = i;
+    }
+        if (i <= currentDay) {
+            document.getElementById(id).classList.add('doorOpened');
+        }*/
 }
 
 function changeLanguage() {
     setTimeout(function () {
-        document.getElementById('language').innerHTML = 'Junus Adventskalender';
+        document.getElementById('language').innerHTML = 'Adventskalender nur für dich';
     }, 0);
 
     setTimeout(function () {
-        document.getElementById('language').innerHTML = 'Junus Advent Calendar';
+        document.getElementById('language').innerHTML = 'Los, freu dich!';
     }, 3000);
 
     setTimeout(function () {
-        document.getElementById('language').innerHTML = 'Junus calendario dell avvento';
+        document.getElementById('language').innerHTML = 'Hab ich nur für dich gemacht!';
     }, 6000);
 
     setTimeout(function () {
-        document.getElementById('language').innerHTML = 'Junus calendrier de lAvent';
+        document.getElementById('language').innerHTML = 'Viel Spaß damit!';
     }, 9000);
 
     setTimeout(function () {
@@ -34,4 +45,6 @@ function openDoor(doorNumber) {
     let id = 'door' + doorNumber;
     document.getElementById(id).classList.add('openDoor');
 }
+
+
 
